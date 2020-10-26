f8e04040680fc1ecaf81b5d915578803f58c534f
Delia Constantino <dconstan92@gmail.com>
2020-10-26 20:42:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,13 @@
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == phrase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
 #
 # def speak_to_grandma(phrase)
 #   if phrase == "I LOVE YOU GRANDMA!"
@@ -8,13 +18,3 @@
 #     return "HUH?! SPEAK UP, SONNY!"
 #   end
 # end
-
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase == phrase
-    return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
-  end
-end
