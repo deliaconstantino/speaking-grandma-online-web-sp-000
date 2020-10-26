e983ada615acb74b54af6a45406652086120bc13
Delia Constantino <dconstan92@gmail.com>
2020-10-26 20:44:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,14 +7,3 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-#
-# def speak_to_grandma(phrase)
-#   if phrase == "I LOVE YOU GRANDMA!"
-#     return "I LOVE YOU TOO PUMPKIN!"
-#   elsif phrase.swapcase == "what?" || phrase.swapcase == "what did you eat today?"
-#     return "NO, NOT SINCE 1938!"
-#   elsif phrase == "Hi!" || phrase == "Hi Nana, how are you?"
-#     return "HUH?! SPEAK UP, SONNY!"
-#   end
-# end
