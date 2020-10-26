75b536dd0fd093c8788fb90bd667abbd20dd41d1
Delia Constantino <dconstan92@gmail.com>
2020-10-26 20:44:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
-  elsif phrase.upcase != phrase
+  else
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
